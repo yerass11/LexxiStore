6ac9aec8db4755fd3f9fcd5d789cb55ad7f8b9ad
Yerassyl Saiman <y_saiman@kbtu.kz>
2024-05-02 04:21:14 +0500

    Initial Commit

---
+++ LexxiStore/LexxiStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  LexxiStoreApp.swift
+//  LexxiStore
+//
+//  Created by Yerasyl on 02.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct LexxiStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
